3e238106b776dc71b7a1232b4f5929fc634aa7ae
Dinar Davis <dinardavis@gmail.com>
2022-06-05 11:57:12 +0700

    Add game logic

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-let computer = ['rock', 'paper', 'scissors']
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+let choices = ['rock', 'paper', 'scissors']
 
 function computerPlay() {
-  return computer[(Math.floor(Math.random() * 3))]
+  return choices[(Math.floor(Math.random() * 3))]
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection);
+  console.log(computerSelection);
+  if(playerSelection === computerSelection) {
+    return ties = ties + 1;
+  } else if((playerSelection === 'rock' && computerSelection === 'scissors') 
+    || (playerSelection === 'paper' && computerSelection === 'rock') 
+    || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+    return playerScore = playerScore + 1;
+  } else if ((computerSelection === 'rock' && playerSelection === 'scissors') 
+    || (computerSelection === 'paper' && playerSelection === 'rock') 
+    || (computerSelection === 'scissors' && playerSelection === 'paper')) {
+    return computerScore = computerScore + 1;
+  }
+}
+
+function game() {
+  console.log(`Player Score: ${playerScore} Computer Score: ${computerScore} with ${ties} ties`)
+  for(let i = 0; i < 5; i++) {
+    const playerSelection = prompt("'Enter Rock, Paper, or Scissors'").toLowerCase();
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log(`Player Score: ${playerScore} Computer Score: ${computerScore} with ${ties} ties`)
+  }
+}
+
+game();
